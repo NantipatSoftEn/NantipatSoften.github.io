9ef68ef6bc926b85954abb8e0320e2c9b1d5b1c5
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-09-15 15:38:58 +0700

    add nav

--- profile-arm/src/App.js
+++ profile-arm/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
 import './css/app.css';
-
+import Header from  './component/Header';
 class App extends Component {
   render() {
     return (
-      <div className="con">
-
+      <div className="container">
+        <Header/>
       </div>
     );
   }
