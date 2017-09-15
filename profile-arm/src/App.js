dcc1d1c743143e484499e3a72f8687bee1cd87b5
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-09-15 18:11:53 +0700

    commit

--- profile-arm/src/App.js
+++ profile-arm/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 
-import './css/app.css';
+
 import Header from  './component/Header';
 class App extends Component {
   render() {
     return (
+        <div className='black'>
       <div className="container">
         <Header/>
       </div>
+      </div>
+
     );
   }
 }
