e8f46a70dbca7fc5075c831b7509fad38e7637c6
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-09-15 15:52:34 +0700

    add bg css

---
+++ profile-arm/src/component/Nav.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Nav extends Component
+{
+    render(){
+        return (
+            <div>
+                <nav classNameName="navbar navbar-default">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <a className="navbar-brand" href="#">WebSiteName</a>
+                        </div>
+                        <ul className="nav navbar-nav">
+                        <li className="active"><a href="#">Home</a></li>
+                        <li><a href="#">Page 1</a></li>
+                        <li><a href="#">Page 2</a></li>
+                        <li><a href="#">Page 3</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        );
+    }
+}
+export default Nav
