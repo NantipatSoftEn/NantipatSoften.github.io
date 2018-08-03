0a8485cf2ae020e5c1769b439a428a6c74fcad73
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2018-08-03 17:20:32 +0700

    re file

--- src/Root.js
+++ src/Root.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         test
       </div>
     );
